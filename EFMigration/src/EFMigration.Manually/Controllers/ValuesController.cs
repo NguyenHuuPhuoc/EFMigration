f9943af0855b7038c6f0fd257d59e65f3e09abc5
Phước <>
2020-08-15 12:34:00 +0700

    EF migration manually

--- EFMigration/src/EFMigration.Manually/Controllers/ValuesController.cs
+++ EFMigration/src/EFMigration.Manually/Controllers/ValuesController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFMigration.Data.Entities;
+using EFMigration.Data.UnitOfWork;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFMigration.Manually.Controllers
 {
@@ -10,36 +13,19 @@ namespace EFMigration.Manually.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        private readonly IUnitOfWorkIntegrationLog _unitOfWork;
+
+        public ValuesController(IUnitOfWorkIntegrationLog unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> Get()
         {
-            return new string[] { "value1", "value2" };
-        }
+            var result = await _unitOfWork.GetRepository<DynamicContent>().Table.ToListAsync();
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return Ok(result);
         }
     }
 }
