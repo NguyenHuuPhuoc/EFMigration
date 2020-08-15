3d4a6829d830ec26a3151a70e8a5d52c8458d34f
Phước <>
2020-08-15 12:58:22 +0700

    Test

--- EFMigration/src/EFMigration.Data/Entities/DynamicContent.cs
+++ EFMigration/src/EFMigration.Data/Entities/DynamicContent.cs
@@ -5,6 +5,6 @@ namespace EFMigration.Data.Entities
     public class DynamicContent : BaseLogEntity
     {
         public ICollection<DynamicContentDetail> DynamicDetails { get; set; }
-        public string Test { get; set; }
+        public string Type { get; set; }
     }
 }
---
+++ EFMigration/src/EFMigration.Data/Migrations/20200815055632_AddFiledTypeInDynamicContent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFMigration.Data.Migrations
+{
+    public partial class AddFiledTypeInDynamicContent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Test",
+                table: "DynamicContents",
+                newName: "Type");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "DynamicContents",
+                newName: "Test");
+        }
+    }
+}
