34015a2cadd64fa44ef45290d0c6ba161cf4d3f5
Phước <>
2020-08-15 13:39:46 +0700

    Add dll and clean

--- EFMigration/src/EFMigration.Data/Entities/DynamicContent.cs
+++ EFMigration/src/EFMigration.Data/Entities/DynamicContent.cs
@@ -6,5 +6,6 @@ namespace EFMigration.Data.Entities
     {
         public ICollection<DynamicContentDetail> DynamicDetails { get; set; }
         public string Type { get; set; }
+        public string Name { get; set; }
     }
 }
--- EFMigration/src/EFMigration.Data/Migrations/20200815063156_AddFieldNameInDynamicContent.Designer.cs
+++ EFMigration/src/EFMigration.Data/Migrations/20200815063156_AddFieldNameInDynamicContent.Designer.cs
@@ -4,14 +4,16 @@ using EFMigration.Data.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EFMigration.Data.Migrations
 {
     [DbContext(typeof(IntegrationLogContext))]
-    partial class IntegrationLogContextModelSnapshot : ModelSnapshot
+    [Migration("20200815063156_AddFieldNameInDynamicContent")]
+    partial class AddFieldNameInDynamicContent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
